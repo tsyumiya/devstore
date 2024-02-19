8fb1bc2868ea844339893b9974665c20ff3b44a9
yaskadju <thales.y@gmail.com>
2024-02-19 09:00:38 -0300

    loading de busca

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -3,7 +3,7 @@ import data from "../data.json"
 import type { NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
-  await new Promise(resolve => setTimeout(resolve, 1000))
+  await new Promise(resolve => setTimeout(resolve, 3000))
 
   const { searchParams } = request.nextUrl
 
