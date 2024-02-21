7d39da126ffc1b66b6a68583b71c932ebc2ce2ee
yaskadju <thales.y@gmail.com>
2024-02-21 17:42:41 -0300

    testando a busca

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,4 +1,8 @@
 describe("add product to cart", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
   it("should be able to navigate to the product page and add it to the cart", () => {
     cy.visit("http://localhost:3000")
     cy.get('a[href^="/product"]').first().click()
